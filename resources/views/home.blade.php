a5a6f10128b3a561133c23e248a9b2e1f45be547
omerkhanjadoon <omerkhanjadoons@gmail.com>
2021-03-05 15:44:12 +0500

    updated font

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -68,7 +68,7 @@
           <footer class="footer">
             <div class="container-fluid clearfix">
               <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © hoopstreet.com 2021</span>
-              <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Developed by<a href="https://www.facebook.com/jadoontechnologies/" target="_blank">  Jadoon Technologies </a></span>
+              
             </div>
           </footer>
           <!-- partial -->
